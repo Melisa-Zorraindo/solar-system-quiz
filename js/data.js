082084dcfeaf5527f2d3c9eb3ea43e3b1d69404f
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-06-10 16:20:27 +0200

    add data

---
+++ js/data.js
@@ -0,0 +1,121 @@
+const questionList = [
+  {
+    image: "../assets/images/nasa-JHyiw_dpALk-sun.jpg",
+    altText: "Close up of the Sun by Nasa",
+    intro:
+      "Our sun, the largest star at the centre of our solar system, is about 4.5 billion years old.",
+    question: "What are its main chemical elements?",
+    options: [
+      "hydrogen and helium",
+      "hydrogen and oxygen",
+      "hydrogen and nitrogen",
+    ],
+    correctAnswer: "hydrogen and helium",
+  },
+
+  {
+    image: "../assets/images/nasa-71W3CWeZF7A-mercury.jpg",
+    altText: "Mercury by Nasa",
+    intro:
+      "Mercury is the fastest planet in our solar system and the closest to the Sun, meaning its year is the shortest of all planets.",
+    question: "How long does a year in Mercury last?",
+    options: ["56 Earth days", "88 Earth days", "121 Earth days"],
+    correctAnswer: "88 Earth days",
+  },
+
+  {
+    image: "../assets/images/nasa-88BMUbQHGlQ-venus.jpg",
+    altText: "Venus by Nasa",
+    intro:
+      "One day in venus is extremely long, it's actually longer than its year, because the planet rotates very slowly on its axis.",
+    question: "How long is a Venusian year?",
+    options: ["354 Earth days", "243 Earth days", "225 Earth days"],
+    correctAnswer: "243 Earth days",
+  },
+
+  {
+    image: "../assets/images/nasa-vhSz50AaFAs-earth.jpg",
+    altText: "Earth by Nasa",
+    intro:
+      "Earth's atmosphere protects the planet from hazardous bodies travelling through space.",
+    question: "What is our atmosphere made of?",
+    options: [
+      "mainly hidrogen and oxygen",
+      "mainly helium and oxygen",
+      "mainly nitrogen and oxygen",
+    ],
+    correctAnswer: "mainly nitrogen and oxygen",
+  },
+
+  {
+    image: "../assets/images/nasa-N3BQHYOVq5E-mars.jpg",
+    altText: "Mars by Nasa",
+    intro:
+      "Mars is a rocky planet known as the Red Planet because minerals in its soil oxidise causing it to look red.",
+    question: "What's the name of these minerals?",
+    options: ["iron", "argon", "oxygen"],
+    correctAnswer: "iron",
+  },
+
+  {
+    image: "../assets/images/nasa-0DjOJ6Q8wUs-jupiter.jpg",
+    altText: "Jupiter by Nasa",
+    intro:
+      "Jupiter is a gas giant and it cannot support life as we know it. However, it is believed that some of its moons could.",
+    question: "How many moons does Jupiter have?",
+    options: ["79", "53", "102"],
+    correctAnswer: "79",
+  },
+
+  {
+    image: "../assets/images/nasa-2W-QWAC0mzI-saturn.jpg",
+    altText: "Saturn and its rings by Nasa",
+    intro:
+      "Saturn is a gas giant, made mostly of hydrogen and helium, well-known for its beautiful seven rings.",
+    question: "What are these rings made of?",
+    options: [
+      "an unknown chemical compound",
+      "iron, hydrogen, and helium",
+      "chunks of ice and rock",
+    ],
+    correctAnswer: "chunks of ice and rock",
+  },
+
+  {
+    image: "../assets/images/nasa-Li41RApUAQA-uranus.jpg",
+    altText: "Uranus by Nasa",
+    intro:
+      "Uranus is the icy giant orbiting in our solar system and it is the seventh planet from our star.",
+    question: "How far is it from the sun?",
+    options: [
+      "3.5 billion kilometers",
+      "2.9 billion kilometers",
+      "1.8 billion kilometers",
+    ],
+    correctAnswer: "2.9 billion kilometers",
+  },
+
+  {
+    image: "../assets/images/nasa-ScBkW9AKgcA-neptune.jpg",
+    altText: "Neptune by Nasa",
+    intro:
+      "Neptune is the only planet in our solar system not visible to the naked eye. It was predicted by mathematics before its discovery.",
+    question: "When was it discovered?",
+    options: ["1846", "1609", "1901"],
+    correctAnswer: "1846",
+  },
+
+  {
+    image: "../assets/images/nasa-V4ZksNimxLk-moon.jpg",
+    altText: "Moon by Nasa",
+    intro:
+      "The moon is Earth's only natural satellite and has an unbreathable, very thin and tenuous atmosphere.",
+    question: "At what distance is it from Earth?",
+    options: [
+      "350 000 kilometers",
+      "4 000 000 kilometers",
+      "239 000 kilometers",
+    ],
+    correctAnswer: "1846",
+  },
+];
