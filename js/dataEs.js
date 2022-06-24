ce2261997607ab6b4d99c18eb2b2a785fbeefa89
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-06-24 21:52:56 +0200

    add data in Spanish

---
+++ js/dataEs.js
@@ -0,0 +1,129 @@
+export const listaPreguntas = [
+  {
+    image: "../assets/images/nasa-JHyiw_dpALk-sun.jpg",
+    altText: "Primer plano del sol, por NASA",
+    intro:
+      "Nuestro sol, la estrella más grande en el centro de nuestro sistema solar, tiene cerca de 4.5 billones de años",
+    question: "¿Qué elementos químicos lo componen mayormente?",
+    options: [
+      "hidrógeno y helio",
+      "hidrógeno y oxígeno",
+      "hidrógeno y nitrógeno",
+    ],
+    correctAnswer: "hidrógeno y helio",
+  },
+
+  {
+    image: "../assets/images/nasa-71W3CWeZF7A-mercury.jpg",
+    altText: "Mercurio, por NASA",
+    intro:
+      "Mercurio es el planeta más rápido de nuestro sistema solar y el más cercano al Sol, lo que significa que su año es el más corto de todos los planetas",
+    question: "¿Cuánto tiempo dura un año en Mercurio?",
+    options: [
+      "56 días terrestres",
+      "88 días terrestres",
+      "121 días terrestres",
+    ],
+    correctAnswer: "88 días terrestres",
+  },
+
+  {
+    image: "../assets/images/nasa-88BMUbQHGlQ-venus.jpg",
+    altText: "Venus, por NASA",
+    intro:
+      "Un año en Venus es extremadamente largo, de hecho, es más largo que su día porque el planeta rota muy lentamente sobre su eje",
+    question: "¿Cuánto dura un año venusiano?",
+    options: [
+      "354 días terrestres",
+      "243 días terrestres",
+      "225 días terrestres",
+    ],
+    correctAnswer: "243 días terrestres",
+  },
+
+  {
+    image: "../assets/images/nasa-vhSz50AaFAs-earth.jpg",
+    altText: "La Tierra, por NASA",
+    intro:
+      "La atmósfera terrestre proteje al planeta de cuerpos peligrosos que viajan a través del espacio",
+    question: "¿De qué está hecha nuestra atmósfera?",
+    options: [
+      "principalmente de hidrógeno y oxígeno",
+      "principalmente de helio y oxígeno",
+      "principalmente de nitrógeno y oxígeno",
+    ],
+    correctAnswer: "principalmente de nitrógeno y oxígeno",
+  },
+
+  {
+    image: "../assets/images/nasa-N3BQHYOVq5E-mars.jpg",
+    altText: "Marte, por NASA",
+    intro:
+      "Marte es un planeta rocoso conocido como el Planeta Rojo porque los minerales de su suelo le dan ese color al oxidarse",
+    question: "¿Cuál es el nombre de esos minerales?",
+    options: ["hierro", "argón", "oxígeno"],
+    correctAnswer: "hierro",
+  },
+
+  {
+    image: "../assets/images/nasa-0DjOJ6Q8wUs-jupiter.jpg",
+    altText: "Júpiter by Nasa",
+    intro:
+      "Júpiter es un gigante gaseoso en el que la vida tal como la conocemos es imposible. Sin embargo, se cree que algunas de sus lunas podrían reunir las condiciones necesarias para la vida",
+    question: "¿Cuántas lunas tiene Júpiter?",
+    options: ["79", "53", "102"],
+    correctAnswer: "79",
+  },
+
+  {
+    image: "../assets/images/nasa-2W-QWAC0mzI-saturn.jpg",
+    altText: "Saturno y sus anillos, por NASA",
+    intro:
+      "Saturno es un gigante gaseoso formado fundamentalmente por hidrógeno y helio, famoso por sus hermosos siete anillos",
+    question: "¿De qué están hechos los anillos?",
+    options: [
+      "un compuesto químico desconocido",
+      "hierro, hidrógeno y helio",
+      "trozos de hielo y roca",
+    ],
+    correctAnswer: "trozos de hielo y roca",
+  },
+
+  {
+    image: "../assets/images/nasa-Li41RApUAQA-uranus.jpg",
+    altText: "Urano, por NASA",
+    intro:
+      "Urano es el gigante de hielo que orbita nuestro sistema solar y es el séptimo planeta desde nuestra estrella",
+    question: "¿Cuán lejos está del Sol?",
+    options: [
+      "3.5 billones de kilómetros",
+      "2.9 billones de kilómetros",
+      "1.8 billones de kilómetros",
+    ],
+    correctAnswer: "2.9 billones de kilómetros",
+  },
+
+  {
+    image: "../assets/images/nasa-ScBkW9AKgcA-neptune.jpg",
+    altText: "Neptuno, por NASA",
+    intro:
+      "Neptuno es el único planeta de nuestro sistema solar que no se puede ver a simple vista. Fue predicho matemáticamente antes de su descubrimiento",
+    question: "¿Cuándo fue descubierto?",
+    options: ["1846", "1609", "1901"],
+    correctAnswer: "1846",
+  },
+
+  {
+    image: "../assets/images/nasa-V4ZksNimxLk-moon.jpg",
+    altText: "La Luna, por NASA",
+    intro:
+      "La luna es el único satélite natural de la Tierra. Su atmósfera es imposible de respirar, muy fina y tenue.",
+    question: "¿A qué distacia de La Tierra se encuentra?",
+    options: [
+      "350 000 kilómetros",
+      "4 000 000 kilómetros",
+      "239 000 kilómetros",
+    ],
+    correctAnswer: "350 000 kilómetros",
+  },
+];
